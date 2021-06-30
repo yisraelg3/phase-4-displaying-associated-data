53b91d6bb4c5f24890456c927a18f976ccb49c74
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-30 17:34:44 -0400

    commit tree ff17b8f25cc80d875270f44180584149a3d67c03

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
 
   def index
-    reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    reviews = Review.all.order(rating: :desc).includes(:dog_house)
+    render json: reviews, include: :dog_house
   end
 
 end
